67109100b1dba0e20d2e17371f918a2c580ca720
ivan <happy.ivan.q@gmail.com>
2025-08-01 19:44:41 +0200

    added interaction method for WinderBlock.java

--- src/main/java/net/vspell/createportable/block/winder/WinderBlock.java
+++ src/main/java/net/vspell/createportable/block/winder/WinderBlock.java
@@ -4,14 +4,31 @@ import com.simibubi.create.content.kinetics.base.RotatedPillarKineticBlock;
 import com.simibubi.create.foundation.block.IBE;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.CampfireCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.CampfireBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.vspell.createportable.CreatePortable;
 import net.vspell.createportable.block.ModBlockEntities;
+
+import java.util.Optional;
 
 public class WinderBlock extends RotatedPillarKineticBlock implements IBE<WinderBlockEntity> {
 
@@ -49,5 +66,12 @@ public class WinderBlock extends RotatedPillarKineticBlock implements IBE<Winder
     @Override
     public BlockEntityType<? extends WinderBlockEntity> getBlockEntityType() {
         return ModBlockEntities.WINDER_ENTITY_ENTRY.get();
+    }
+
+    protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        BlockEntity itemstack = level.getBlockEntity(pos);
+        CreatePortable.LOGGER.info("A WINDER HAS BEEN CLICKED");
+
+        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
 }
